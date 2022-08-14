3e4a068797f34fff99e2047880d559793aa7b280
Abel NZI <abelnzi@gmail.com>
2022-08-14 09:59:03 +0000

    update image path

--- components/Layout.js
+++ components/Layout.js
@@ -42,7 +42,7 @@ function Layout({ children, page }) {
             <a>
               <div className="mx-4 my-2 cursor-pointer">
                 <Image
-                  src={`${process.env.NEXT_PUBLIC_PATH}/logo.png`}
+                  src={`${process.env.NEXT_PUBLIC_PATH}/imgs/logo.png`}
                   alt="Logo"
                   width={150}
                   height={70}
