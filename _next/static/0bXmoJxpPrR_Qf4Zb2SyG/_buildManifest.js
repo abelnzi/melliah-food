d5450f0c5f28d46d3535ebec20556aff005a722e
abelnzi <abelnzi@users.noreply.github.com>
2022-08-14 09:59:53 +0000

    Deploying to gh-pages from @ abelnzi/melliah-food@3e4a068797f34fff99e2047880d559793aa7b280 🚀

--- _next/static/0bXmoJxpPrR_Qf4Zb2SyG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/402-86b37c4b4358ef81.js","static/chunks/pages/index-a2671b1659badf3d.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
